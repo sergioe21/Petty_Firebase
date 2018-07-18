9c0be241fc8dc037fc32998591d1439ead7ed1f5
Sergio Esteban <sergioesteban@MacBook-Pro-de-Sergio.local>
2018-07-18 12:36:29 -0500

    Add Firebase

--- Petty/AppDelegate.swift
+++ Petty/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+let db = Firestore.firestore()
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
 
--- Petty/TableViewController.swift
+++ Petty/TableViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+var datos = [String]()
 
 class TableViewController: UITableViewController {
-
+    
+    var refresher: UIRefreshControl!
+    
     override func viewDidLoad() {
+        
+        db.collection("Users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    //print("\(document.documentID) => \(document.data())")
+                    var NewItem = document.data()["name"] as! String
+                    print(NewItem)
+                    datos.append(NewItem)
+                }
+            }
+            self.tableView.reloadData()
+        }
+        
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,29 +44,59 @@ class TableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return datos.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
         // Configure the cell...
+        cell.textLabel?.text = datos[indexPath.row]
+        if indexPath.row % 2 == 0{
+            cell.backgroundColor = UIColor.lightGray
+        }else{
+            cell.backgroundColor = UIColor.white
+        }
 
         return cell
     }
-    */
-
+    @IBAction func AddingItem(_ sender: Any) {
+        print("Adding")
+        
+        let alert = UIAlertController(title: "Nuevo Usuario", message: "Ingrese nuevo usuario" , preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Ingrese el nombre"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { (action) in
+            let newUser: String = (alert.textFields?.first?.text)!
+            
+            var ref: DocumentReference? = nil
+            
+            ref = db.collection("Users").addDocument(data: [
+                "name": newUser
+                ])
+            datos.append(newUser)
+            self.tableView.reloadData()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .destructive, handler: {(action) -> Void in
+        }))
+        
+        self.present(alert,animated: true,completion: nil)
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
